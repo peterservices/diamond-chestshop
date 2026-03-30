ee1f262f721ea01c3a1409c638696f48aeb520d0
Ticataco <148506232+peterservices@users.noreply.github.com>
2026-03-29 21:52:25 -0600

    Make variable names more intuitive

--- src/main/java/com/gmail/sneakdevs/diamondchestshop/mixin/HopperBlockEntityMixin.java
+++ src/main/java/com/gmail/sneakdevs/diamondchestshop/mixin/HopperBlockEntityMixin.java
@@ -29,11 +29,11 @@ public class HopperBlockEntityMixin {
     }
 
     @Inject(method = "canTakeItemFromContainer", at = @At("HEAD"), cancellable = true)
-    private static void diamondchestshop_addItemMixin(Container container, Container container2, ItemStack itemStack, int i, Direction direction, CallbackInfoReturnable<Boolean> cir) {
-        if (DiamondChestShopConfig.getInstance().shopProtectHopper && container2 instanceof BaseContainerBlockEntity && ((BaseContainerBlockEntityInterface)container2).diamondchestshop_getShop()) {
+    private static void diamondchestshop_addItemMixin(Container intoContainer, Container fromContainer, ItemStack itemStack, int i, Direction direction, CallbackInfoReturnable<Boolean> cir) {
+        if (DiamondChestShopConfig.getInstance().shopProtectHopper && fromContainer instanceof BaseContainerBlockEntity && ((BaseContainerBlockEntityInterface)fromContainer).diamondchestshop_getShop()) {
             cir.setReturnValue(false);
-        } else if (DiamondChestShopConfig.getInstance().shopProtectHopper && container2 instanceof CompoundContainer) {
-            CompoundContainerAccessor accessor = (CompoundContainerAccessor)container2;
+        } else if (DiamondChestShopConfig.getInstance().shopProtectHopper && fromContainer instanceof CompoundContainer) {
+            CompoundContainerAccessor accessor = (CompoundContainerAccessor)fromContainer;
             if (((BaseContainerBlockEntityInterface)accessor.getContainer1()).diamondchestshop_getShop() || ((BaseContainerBlockEntityInterface)accessor.getContainer2()).diamondchestshop_getShop()) {
                 cir.setReturnValue(false);
             }

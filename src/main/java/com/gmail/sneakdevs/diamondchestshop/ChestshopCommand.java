fec36d2b6cc4f5e604780bc9f825669e74315c45
Ticataco <148506232+peterservices@users.noreply.github.com>
2026-08-13 21:26:28 -0600

    Revise chestshop command text

--- src/main/java/com/gmail/sneakdevs/diamondchestshop/ChestshopCommand.java
+++ src/main/java/com/gmail/sneakdevs/diamondchestshop/ChestshopCommand.java
@@ -31,14 +31,14 @@ public class ChestshopCommand {
     private static int chestshopCommand(CommandContext<CommandSourceStack> ctx) {
         ctx.getSource().sendSuccess(() -> Component.literal(
                 "To create a chest shop: \n" +
-                        "1) place a chest with a sign attached \n" +
-                        "2) write \"buy\" or \"sell\" on the first line \n" +
-                        "3) write the quantity of the item to be exchanged on the second line \n" +
-                        "4) write the amount of currency to be exchanged on the third line \n" +
-                        "5) hold the item to sell in your offhand and click the sign with a " + DiamondEconomyConfig.getCurrencyName(0) + "\n\n" +
-                        "To buy/sell from a chestshop punch or right click the sign.\n" +
-                        "If selling to the shop make sure the items are in your inventory and punch a shop's sign that contains \"buy\" on the first line.\n" +
-                        "If buying from a shop make sure you have money in your account and right click a shop's sign that contains \"sell\" on the first line."
+                        "1) Place a chest with a sign attached \n" +
+                        "2) Write \"buy\" or \"sell\" on the first line \n" +
+                        "3) Write the quantity of the item to be exchanged on the second line \n" +
+                        "4) Write the amount of currency to be exchanged on the third line \n" +
+                        "5) Hold the item to sell in your offhand and click the sign with a " + DiamondEconomyConfig.getCurrencyName(0) + " \n\n" +
+                        "To buy/sell from a chestshop, punch or right click the sign. \n" +
+                        "If selling to the shop, make sure the items are in your inventory and punch a shop's sign that contains \"buy\" on the first line. \n" +
+                        "If buying from a shop, make sure you have money in your account and right click a shop's sign that contains \"sell\" on the first line. "
                 ),
                 false);
         return 1;
